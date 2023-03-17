2bea1d686543f598fbd96f738759ac3509227dd0
lambert34 <aleksandrlambert@gmail.com>
2023-03-17 19:18:42 +0700

    доделал макет
    доделал слайдер и финальную секцию

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(function(){
 $('.design-slider').slick({
-    dots: true,
-    
+    dots: false,
+    slidesToShow: 4,
+    variableWidth: true
+    prevArrow: '<img class="arrow arrow-left" src="images/slider/Arrow2.svg" alt="левая">'
+    nextArrow: '<img class="arrow arrow-left" src="images/slider/Arrow1.svg" alt="правая">'
 });
 });
