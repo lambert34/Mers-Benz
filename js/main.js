0466bbf97128cb66582c10269e69bd8e5b70a2b5
lambert34 <aleksandrlambert@gmail.com>
2023-03-23 16:39:46 +0700

    добавил адаптив с помощью медиа-запросов

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 $(function(){
 $('.design-slider').slick({
-    dots: false,
+    dots: true,
     slidesToShow: 4,
     variableWidth: true
-    prevArrow: '<img class="arrow arrow-left" src="images/slider/Arrow2.svg" alt="левая">'
-    nextArrow: '<img class="arrow arrow-left" src="images/slider/Arrow1.svg" alt="правая">'
 });
 });
+
+// prevArrow: '<img class="arrow arrow-left" src="images/slider/Arrow2.svg" alt="левая">'
+//     nextArrow: '<img class="arrow arrow-right" src="images/slider/Arrow1.svg" alt="правая">'
